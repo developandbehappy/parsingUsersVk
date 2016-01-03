a6a1211fb0c3e4d7d3eba6756b3fc0a7fd6beee2
AveVlad <dev@vld.me>
2016-01-04 00:00:23 +0300

    last update

--- src/controllers/aboutCtrl.js
+++ src/controllers/aboutCtrl.js
@@ -1,35 +1,41 @@
 vkApp.controller('AboutCtrl', function ($scope, $http, $q, vkApiService, vkFetchDataService) {
-  $scope.groupId = '2';
+  $scope.groupId = '-74598451';
+//  $scope.groupId = '80651295'; // zd
   $scope.showloadingStats = false;
+  $scope.isShowlistPeople = false;
   $scope.totalPost = 0;
   $scope.totalLikes = 0;
   $scope.totalPeople = 0;
+  $scope.finishResultList = [];
   $scope.searchBtnHandler = function () {
     destroyData();
     var idWall = $scope.groupId;
     var fetchWallData = vkFetchDataService.fetchWallData(idWall, 10000, 20);
     $scope.showloadingStats = true;
     var fetchPostLikeDataPromise = fetchWallData.then(function (response) {
-      return vkFetchDataService.fetchPostLikeData(response);
+      console.log('response', response);
+      return vkFetchDataService.fetchLikesDataLess1k(response, 23);
     }, function (error) {
       console.log('error', error);
       return false;
     }, function (notify) {
-      console.log('notify', notify);
       $scope.totalPost = formatNumber($scope.totalPost, notify.postCount);
       $scope.totalLikes = formatNumber($scope.totalLikes, notify.likesCount);
       return false;
     });
 
     fetchPostLikeDataPromise.then(function (response) {
-      console.log('wwww', response);
+      var res = sortLikes(response);
+      $scope.finishResultList = res;
+      console.log('$scope.finishResultList', $scope.finishResultList);
+      console.log('res', res);
+      $scope.isShowlistPeople = true;
     }, function (error) {
 
     }, function (notify) {
       if (!notify) {
         return false;
       }
-      console.info('notify.count', notify);
       $scope.totalPeople = formatNumber($scope.totalPeople, notify.count);
     });
   };
@@ -38,18 +44,37 @@ vkApp.controller('AboutCtrl', function ($scope, $http, $q, vkApiService, vkFetch
     $scope.showloadingStats = false;
     $scope.totalPost = 0;
     $scope.totalLikes = 0;
+    $scope.isShowlistPeople = false;
+    $scope.finishResultList = [];
   };
   var replaceAll = function (str, s, r) {
     return str.toString().split(s).join(r);
   };
   var formatNumber = function (prevVal, currentVal) {
-    console.log('prevVal', prevVal);
     prevVal = replaceAll(prevVal, ' ', '');
     prevVal = parseInt(prevVal);
-    console.log('prevVal', prevVal);
     var num = currentVal + prevVal;
     return accounting.formatNumber(num, 0, " ");
   };
+//
+  var sortLikes = function (list) {
+    var userLikesResultObject = _.countBy(list);
+    var arr = [];
+    for (var prop in userLikesResultObject) {
+      if (userLikesResultObject.hasOwnProperty(prop)) {
+        arr.push({
+          'key': prop,
+          'value': userLikesResultObject[prop]
+        });
+      }
+    }
+    var result = _.sortBy(arr, function (item) {
+      return -item.value;
+    });
+    return result.slice(0, 100);
+  };
+//
+//  console.log('userLikesResult', _.countBy(userLikesResult));
 //  var getLengthPosts = function (groupId) {
 //    var countMax = 0;
 //    var url = vkApiService.wallGet({
--- src/factory/vkFetchData.service.js
+++ src/factory/vkFetchData.service.js
@@ -1,23 +1,29 @@
 vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
+  var api = {
+    wallGet: function wallGet(opt) {
+      opt = opt || {};
+      opt.count = 100;
+      var queryString = JSON.stringify(opt);
+      return "API.wall.get(" + queryString + ")";
+    },
+    likeGet: function likeGet(opt) {
+      opt = opt || {};
+      opt.type = 'post';
+      var queryString = JSON.stringify(opt);
+      return "API.likes.getList(" + queryString + ")";
+    }
+  };
+
   return {
     fetchWallData: function (groupId, postsSize, streamCount) {
-      console.log('groupId', groupId);
       var deferred = $q.defer();
       var postsLength = postsSize;
-      var api = {
-        wallGet: function wallGet(opt) {
-          opt = opt || {};
-          return "API.wall.get(" +
-            "{\"owner_id\": " + opt.owner_id + ", \"offset\": " + opt.offset + ", \"count\": 100}" + ")";
-        }
-      };
       var vkScriptRequestList = [];
       var resultList = [];
       for (var i = 0; i < postsLength; i += 100) {
         var vkScriptRequest = api.wallGet({owner_id: groupId, offset: i});
         vkScriptRequestList.push(vkScriptRequest);
       }
-      console.log('vkScriptRequestList', vkScriptRequestList.length);
       var getData = function () {
         if (vkScriptRequestList.length === 0) {
           deferred.resolve(finishResponseFilter(resultList));
@@ -28,7 +34,6 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
         vkApiService.execute({
           code: vkScriptCode
         }).then(function (response) {
-          console.log('response', response);
           var tempResult = [];
           var nextStep = true;
           response.data.response.forEach(function (item) {
@@ -40,8 +45,6 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
               nextStep = false;
             }
           });
-          console.log('tempResult', tempResult);
-          console.log('tempResult', _.size(tempResult));
           var notifyData = finishNotifyFilter({
             type: 'temp',
             data: tempResult
@@ -58,12 +61,14 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
       };
       getData();
       var finishResponseFilter = function (list) {
-        console.log('list', list);
         var wallDataResult = list.reduce(function (previousValue, currentItem) {
 
           return previousValue.concat(currentItem);
         }).reduce(function (previousValue, currentItem) {
           return previousValue.concat(currentItem);
+        });
+        wallDataResult = wallDataResult.filter(function (item) {
+          return item.likes.count > 0;
         });
         wallDataResult = wallDataResult.map(function (item) {
           return {
@@ -92,21 +97,22 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
       return deferred.promise;
     },
     fetchLikesData: function (groupId, postItemId, likeSize, streamCount) {
+      var finishResponseFilter = function (list) {
+        return list.reduce(function (previousValue, currentItem) {
+          return previousValue.concat(currentItem);
+        });
+      };
       var count = 1000;
       var deferred = $q.defer();
-      var api = {
-        likeGet: function likeGet(opt) {
-          opt = opt || {};
-          return "API.likes.getList(" +
-            "{\"owner_id\": " + opt.owner_id + ", \"offset\": " + opt.offset + "," +
-            " \"item_id\": " + opt.item_id + "," +
-            " \"type\": \"post\", \"count\": " + count + "}" + ")";
-        }
-      };
       var vkScriptRequestList = [];
       var arrData = [];
       for (var i = 0; i < likeSize; i += count) {
-        var vkScriptRequest = api.likeGet({owner_id: groupId, offset: i, item_id: postItemId});
+        var vkScriptRequest = api.likeGet({
+          owner_id: groupId,
+          offset: i,
+          count: count,
+          item_id: postItemId
+        });
         vkScriptRequestList.push(vkScriptRequest);
       }
       var getData = function () {
@@ -130,11 +136,6 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
         });
       };
       getData();
-      var finishResponseFilter = function (list) {
-        return list.reduce(function (previousValue, currentItem) {
-          return previousValue.concat(currentItem);
-        });
-      };
       return deferred.promise;
     },
     fetchPostLikeData: function (wallDataList) {
@@ -173,6 +174,55 @@ vkApp.factory('vkFetchDataService', function (vkApiService, $q) {
         return {
           count: _.size(opt.data.response)
         };
+      };
+
+      return deferred.promise;
+    },
+    fetchLikesDataLess1k: function (postIdList, streamCount) {
+      var postList = postIdList.filter(function (item) {
+        var likeSize = item.likeCount;
+        return likeSize > 0 && likeSize <= 1000;
+      });
+      var deferred = $q.defer();
+      var vkScriptRequestList = [];
+      var resultList = [];
+      postList.forEach(function (item) {
+        var vkScriptRequest = api.likeGet({
+          owner_id: item.groupId,
+          offset: 0,
+          count: 1000,
+          item_id: item.postId
+        });
+        vkScriptRequestList.push(vkScriptRequest);
+      });
+      var getData = function () {
+        if (vkScriptRequestList.length === 0) {
+          deferred.resolve(finishResponseFilter(resultList));
+          return false;
+        }
+        var dataForRequest = vkScriptRequestList.splice(0, streamCount);
+        var vkScriptCode = "return [" + dataForRequest.join() + "];";
+        vkApiService.execute({
+          code: vkScriptCode
+        }).then(function (response) {
+          var preResultData = response.data.response;
+          preResultData = preResultData.map(function (item) {
+            return item.users;
+          }).reduce(function (previousValue, currentItem) {
+            return previousValue.concat(currentItem);
+          });
+          console.log('preResultData', preResultData);
+          resultList.push(preResultData);
+          setTimeout(function () {
+            getData();
+          }, 600);
+        });
+      };
+      getData();
+      var finishResponseFilter = function (list) {
+        return list.reduce(function (previousValue, currentItem) {
+          return previousValue.concat(currentItem);
+        });
       };
 
       return deferred.promise;
