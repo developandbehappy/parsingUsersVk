6d7da09690dabd9d2dcba9d616f7db3f439efaef
zidein07 <zidein07@gmail.com>
2016-01-04 22:02:40 +0400

    upd

--- src/directive/translate-select.directive.js
+++ src/directive/translate-select.directive.js
@@ -1,5 +1,14 @@
-vkApp.directive('wallParserInfo', function () {
+vkApp.directive('translateSelect', function () {
   return {
-
+    restrict: 'E',
+    templateUrl: '/src/directive/base/select.html',
+    controller: [
+      '$scope', '$translate', 'localStorageService',
+      function ($scope, $translate) {
+        $scope.translateSelect = $translate.use();
+        $scope.changeSelect = function () {
+          $translate.use($scope.translateSelect);
+        };
+      }]
   };
 });
