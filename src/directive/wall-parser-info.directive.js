b10b2c2fd4d4bcd74a5eb46ece9f1fd94281ef3c
zidein07 <zidein07@gmail.com>
2016-01-05 11:56:10 +0400

    upd parse link

--- src/directive/wall-parser-info.directive.js
+++ src/directive/wall-parser-info.directive.js
@@ -9,21 +9,42 @@ vkApp.directive('wallParserInfo', function (vkFetchDataService) {
     link: function (scope) {
       scope.info = false;
       scope.infoStatus = false;
-      var parseLink = function (link) {
-        if (_.isEmpty(link)) {
+      var parsePageGroup = function (urlPage) {
+        if (_.isEmpty(urlPage)) {
           return false;
         }
-        if (!isNaN(Number(link))) {
-          return 'user'
+        var typePage = '';
+        var groupLink = urlPage.replace(/\D+/g, '');
+        console.log('groupLink', groupLink);
+        if (urlPage.indexOf('id') + 1) {
+          console.log('id found!');
+          typePage = 'user';
+        }
+        if (urlPage.indexOf('club') + 1) {
+          console.log('club found');
+          typePage = 'slug';
+        }
+        if (urlPage.indexOf('public') + 1) {
+          typePage = 'slug'
+        }
+        if (urlPage.indexOf('event') + 1) {
+          typePage = 'slug'
         } else {
-          return 'slug'
+          return {
+            type: 'slug',
+            idPage: urlPage
+          };
+        }
+        return {
+          type: typePage,
+          idPage: groupLink
         }
       };
       scope.$watch('control', function (currentValue, prevValue) {
         //console.log('currentValue', currentValue);
 //        parseLink(currentValue);
         scope.infoStatus = true;
-        vkFetchDataService.fetchPageInfo(currentValue, parseLink(currentValue)).then(function (res) {
+        vkFetchDataService.fetchPageInfo(parsePageGroup(currentValue).idPage, parsePageGroup(currentValue).type).then(function (res) {
           //console.log('res', res);
           scope.info = res;
         });
