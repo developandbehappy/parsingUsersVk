936ee5a8c82d688182f46f32fb9c690f2bdaa454
zidein07 <zidein07@gmail.com>
2016-01-05 12:17:31 +0400

    upd link

--- src/directive/wall-parser-info.directive.js
+++ src/directive/wall-parser-info.directive.js
@@ -13,27 +13,26 @@ vkApp.directive('wallParserInfo', function (vkFetchDataService) {
         if (_.isEmpty(urlPage)) {
           return false;
         }
-        var typePage = '';
-        var groupLink = urlPage.replace(/\D+/g, '');
+        var groupLink = urlPage;
+        var typePage = 'slug';
         console.log('groupLink', groupLink);
         if (urlPage.indexOf('id') + 1) {
           console.log('id found!');
+          groupLink = urlPage.replace(/\D+/g, '');
           typePage = 'user';
         }
         if (urlPage.indexOf('club') + 1) {
           console.log('club found');
-          typePage = 'slug';
+          groupLink = urlPage.replace(/\D+/g, '');
+          typePage = 'group';
         }
         if (urlPage.indexOf('public') + 1) {
-          typePage = 'slug'
+          typePage = 'group'
+          groupLink = urlPage.replace(/\D+/g, '');
         }
         if (urlPage.indexOf('event') + 1) {
-          typePage = 'slug'
-        } else {
-          return {
-            type: 'slug',
-            idPage: urlPage
-          };
+          typePage = 'group'
+          groupLink = urlPage.replace(/\D+/g, '');
         }
         return {
           type: typePage,
