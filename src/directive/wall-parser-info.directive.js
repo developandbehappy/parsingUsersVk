1660ca6d64ce744935bd610a2ea37e8700089908
zidein07 <zidein07@gmail.com>
2016-01-05 14:40:56 +0400

    update

--- src/directive/wall-parser-info.directive.js
+++ src/directive/wall-parser-info.directive.js
@@ -10,32 +10,40 @@ vkApp.directive('wallParserInfo', function (vkFetchDataService) {
       scope.info = false;
       scope.infoStatus = false;
       var parsePageGroup = function (urlPage) {
+        var arrUrlType = {
+          id: 'user',
+          group: 'group',
+          slug: 'slug'
+        };
+
         if (_.isEmpty(urlPage)) {
           return false;
         }
+        var splitLink = '';
         var groupLink = urlPage;
         var typePage = 'slug';
-        if(!isNaN(Number(urlPage))) {
-          typePage = 'user';
+        if (!isNaN(Number(urlPage))) {
+          typePage = arrUrlType.id;
         }
-        console.log('groupLink', groupLink);
         if (urlPage.indexOf('id') + 1) {
-          console.log('id found!');
-          groupLink = urlPage.replace(/\D+/g, '');
-          typePage = 'user';
+          splitLink = urlPage.split('id');
+          groupLink = splitLink[1];
+          typePage = arrUrlType.id;
         }
         if (urlPage.indexOf('club') + 1) {
-          console.log('club found');
-          groupLink = urlPage.replace(/\D+/g, '');
+          splitLink = urlPage.split('club');
+          groupLink = splitLink[1];
           typePage = 'group';
         }
         if (urlPage.indexOf('public') + 1) {
-          typePage = 'group'
-          groupLink = urlPage.replace(/\D+/g, '');
+          typePage = 'group';
+          splitLink = urlPage.split('public');
+          groupLink = splitLink[1];
         }
         if (urlPage.indexOf('event') + 1) {
-          typePage = 'group'
-          groupLink = urlPage.replace(/\D+/g, '');
+          typePage = 'group';
+          groupLink = splitLink[1];
+          splitLink = urlPage.split('event');
         }
         return {
           type: typePage,
