51dc85221dfb881452f3ca7b3c0f51afaa6e13b6
zidein07 <zidein07@gmail.com>
2016-01-05 17:33:31 +0400

    upd

--- spec/jasmine_examples/wallParserLinkSpec.js
+++ spec/jasmine_examples/wallParserLinkSpec.js
@@ -4,19 +4,78 @@ describe("wallParserLinkSpec", function () {
   var parseLink = function (link) {
     var validateReplaceRes = link.replace(/[\w|:|\/|.]+/, '');
     var validateStatus = validateReplaceRes.length === 0;
+    var splitLink = '';
+    var idPage = '';
     if (!validateStatus || _.isEmpty(link)) {
       return {
         validate: false
       }
     }
     if (!isNaN(Number(link))) {
+      idPage = parseInt(link);
       return {
-        id: link,
+        id: idPage,
         type: 'user',
         validate: true
       }
     }
-    
+    else if (_.include(link, 'id')) {
+      splitLink = link.split('id');
+      idPage = parseInt(splitLink[1]);
+      return {
+        id: idPage,
+        type: 'user',
+        validate: true
+      }
+    }
+    else if (_.include(link, 'club')) {
+      splitLink = link.split('club');
+      idPage = parseInt(splitLink[1]);
+      if (!idPage) {
+        return {
+          id: 'club',
+          type: 'slug',
+          validate: true
+        }
+      }
+      return {
+        id: idPage,
+        type: 'group',
+        validate: true
+      }
+    }
+    else if (_.include(link, 'event')) {
+      splitLink = link.split('event');
+      idPage = parseInt(splitLink[1]);
+      return {
+        id: idPage,
+        type: 'group',
+        validate: true
+      }
+    }
+    else if (_.include(link, 'public')) {
+      splitLink = link.split('public');
+      idPage = parseInt(splitLink[1]);
+      return {
+        id: idPage,
+        type: 'group',
+        validate: true
+      }
+    } else if (_.include(link, 'vk.com/')) {
+      splitLink = link.split('vk.com/');
+      idPage = splitLink[1];
+      return {
+        id: idPage,
+        type: 'slug',
+        validate: true
+      }
+    } else {
+      return {
+        id: link,
+        type: 'slug',
+        validate: true
+      }
+    }
 
     return {
       validate: true
@@ -34,14 +93,14 @@ describe("wallParserLinkSpec", function () {
   });
   it("1", function () {
     expect(parseLink('1')).toEqual({
-      id: '1',
+      id: 1,
       type: 'user',
       validate: true
     });
   });
   it("https://vk.com/id1", function () {
     expect(parseLink('https://vk.com/id1')).toEqual({
-      id: '1',
+      id: 1,
       type: 'user',
       validate: true
     });
@@ -60,6 +119,13 @@ describe("wallParserLinkSpec", function () {
       validate: true
     });
   });
+  it("https://vk.com/club", function () {
+    expect(parseLink('https://vk.com/club')).toEqual({
+      id: 'club',
+      type: 'slug',
+      validate: true
+    });
+  });
   it("https://vk.com/event100", function () {
     expect(parseLink('https://vk.com/event100')).toEqual({
       id: 100,
@@ -74,24 +140,62 @@ describe("wallParserLinkSpec", function () {
       validate: true
     });
   });
-//  it("vk.com/public100", function () {
-//    expect(parseLink('vk.com/public100')).toEqual({test: 123});
-//  });
-//  it("vkontakte.ru/public100", function () {
-//    expect(parseLink('vkontakte.ru/public100')).toEqual({test: 123});
-//  });
-//  it("club123", function () {
-//    expect(parseLink('club123')).toEqual({test: 123});
-//  });
-//  it("public123", function () {
-//    expect(parseLink('public123')).toEqual({test: 123});
-//  });
-//  it("event123", function () {
-//    expect(parseLink('event123')).toEqual({test: 123});
-//  });
-//  it("id1", function () {
-//    expect(parseLink('id1')).toEqual({test: 123});
-//  });
+  it("vk.com/public100", function () {
+    expect(parseLink('vk.com/public100')).toEqual({
+      id: 100,
+      type: 'group',
+      validate: true
+    });
+  });
+  it("vkontakte.ru/public100", function () {
+    expect(parseLink('vkontakte.ru/public100')).toEqual({
+      id: 100,
+      type: 'group',
+      validate: true
+    });
+  });
+  it("vkontakte.ru/durov", function () {
+    expect(parseLink('vkontakte.ru/durov')).toEqual({
+      id: 'durov',
+      type: 'slug',
+      validate: true
+    });
+  });
+  it("vk.com/durov", function () {
+    expect(parseLink('vk.com/durov')).toEqual({
+      id: 'durov',
+      type: 'slug',
+      validate: true
+    });
+  });
+  it("club123", function () {
+    expect(parseLink('club123')).toEqual({
+      id: 123,
+      type: 'group',
+      validate: true
+    });
+  });
+  it("public777", function () {
+    expect(parseLink('public777')).toEqual({
+      id: 777,
+      type: 'group',
+      validate: true
+    });
+  });
+  it("event555", function () {
+    expect(parseLink('event555')).toEqual({
+      id: 555,
+      type: 'group',
+      validate: true
+    });
+  });
+  it("id1", function () {
+    expect(parseLink('id1')).toEqual({
+      id: 1,
+      type: 'user',
+      validate: true
+    });
+  });
 
   it("https://vk.com/d%123", function () {
     expect(parseLink('https://vk.com/d%123')).toEqual(validateFalse);
