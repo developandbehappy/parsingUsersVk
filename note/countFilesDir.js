cc5fa6c3c0d5303fd2776eb598dbc1b41d18485a
zidein07 <zidein07@gmail.com>
2016-01-06 21:29:09 +0400

    node create func which gimme counts files I

--- note/countFilesDir.js
+++ note/countFilesDir.js
@@ -1,21 +1,21 @@
 var fs = require('fs');
 
-function getFiles (dir, files_, files_count){
+function getFiles(dir, files_, files_count) {
   files_ = files_ || [];
   var files = fs.readdirSync(dir);
   console.log('files', files);
-  for (var i in files){
+  for (var i in files) {
     var name = dir + '/' + files[i];
-    if (fs.statSync(name).isDirectory()){
+    if (fs.statSync(name).isDirectory()) {
       getFiles(name, files_, files_count);
     } else {
       files_.push(name);
     }
   }
-//  return {
-//    files_count: files_.length,
-//    files: files_
-//  };
+  return {
+    files_count: files_.length,
+    files: files_
+  };
 }
 
-getFiles('src');
+console.log(getFiles('src'));
